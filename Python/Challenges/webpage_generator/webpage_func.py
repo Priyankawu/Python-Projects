eb091a150f193e54a97303ee25c47852054c2ac2
Priyanka Thakan <priyankawu@yahoo.com>
2021-01-27 18:08:29 -0800

    Update webpage-generator

--- Python/Challenges/webpage_generator/webpage_func.py
+++ Python/Challenges/webpage_generator/webpage_func.py
@@ -1,5 +1,6 @@
 import webbrowser
 
+#create html file with user text and display in webbrowser.
 def createHTML_file(text):
     #create a file if it does not exist
     f=open("htmlfile.html","w")
@@ -16,7 +17,7 @@ def createHTML_file(text):
     f.write(html_string)
     f.close()
     #notice the r before the path: w/o it, it doesn't work
-    url = r'C:\Users\sanjeev\GitHub\Python Projects\Python\Challenges\htmlfile.html'
+    url = 'htmlfile.html'
     webbrowser.open_new_tab(url) # 0 says to open in same browser, 1 opens new browser
 
 if __name__=="__main__":
--- Python/Challenges/webpage_generator/webpage_gui.py
+++ Python/Challenges/webpage_generator/webpage_gui.py
@@ -3,16 +3,17 @@ from tkinter import * #w/o this, sticky does not work
 import webpage_main
 import webpage_func
 
-
+#This function makes the entry box and the create button that creates the html file
+# and opens it in the browser.
 def load_gui(self):
 
-    self.entry_box = tk.Entry(self.master,text="", bg="light gray")
-    #? row, column, sticky are not in right place in gui
-    self.entry_box.grid(row=0, column=0)
+    self.entry_box = tk.Entry(self.master,text="", bg="light gray", width = 45)
+
+    self.entry_box.grid(row=0, column=0, padx=(10,10), pady=(30,20))
 
     self.bt = tk.Button(self.master,text="Create", width=15,
                 height=2,command=lambda:webpage_func.createHTML_file(self.entry_box.get()))
-    self.bt.grid(row=1, column=0)
+    self.bt.grid(row=1, padx=(10,10),column=0)
 
    
 
